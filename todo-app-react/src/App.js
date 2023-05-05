05f82c7aaf63a58b5b48d276000859a53cfe5b3f
DenizChildir <73806328+DenizChildir@users.noreply.github.com>
2023-05-05 14:00:01 -0700

    react

--- todo-app-react/src/App.js
+++ todo-app-react/src/App.js
@@ -1,143 +1,228 @@
-import { useState, useEffect} from 'react';
-
-import DraggedTaskContext from './DraggedTaskContext';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import SearchInput from "./SearchInput";
-import TaskControls from "./TaskControls";
-import TaskList from "./TaskList";
 
 function App() {
     const [tasks, setTasks] = useState([]);
+    const [inputValue, setInputValue] = useState('');
     const [filter, setFilter] = useState('all');
-    const [search, setSearch] = useState('');
-    const [draggedTask, setDraggedTask] = useState(null);
+    const [searchText, setSearchText] = useState('');
+    const [showConfirmModal, setShowConfirmModal] = useState(false);
+    const [taskIndexToDelete, setTaskIndexToDelete] = useState(null);
 
     useEffect(() => {
-        loadTasksFromLocalStorage();
+        const tasksJSON = localStorage.getItem('tasks');
+        if (tasksJSON) {
+            setTasks(JSON.parse(tasksJSON));
+        }
     }, []);
 
     useEffect(() => {
-        saveTasksToLocalStorage();
-    }, [tasks, saveTasksToLocalStorage]);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }, [tasks]);
 
-    function addTask(taskText) {
-        if (taskText.trim() === '') {
+    const addTask = () => {
+        const taskText = inputValue.trim();
+        if (taskText === '') {
             alert('Enter a task');
             return;
         }
-        setTasks([...tasks, { text: taskText, completed: false }]);
-    }
+        setTasks([...tasks, {text: taskText, completed: false, priority: 'low'}]);
+        setInputValue('');
+    };
 
-    function deleteTask(taskIndex) {
-        setTasks(tasks.filter((_, index) => index !== taskIndex));
-    }
-
-    function toggleTaskCompletion(taskIndex) {
+    const toggleTaskCompletion = (index) => {
         setTasks(
-            tasks.map((task, index) =>
-                index === taskIndex
-                    ? { ...task, completed: !task.completed }
-                    : task
+            tasks.map((task, i) =>
+                i === index ? {...task, completed: !task.completed} : task
             )
         );
-    }
+    };
 
-    function editTask(taskIndex, newTaskText) {
-        if (newTaskText.trim() === '') {
-            deleteTask(taskIndex);
-        } else {
-            setTasks(
-                tasks.map((task, index) =>
-                    index === taskIndex ? { ...task, text: newTaskText } : task
-                )
-            );
-        }
-    }
-    function clearTasks() {
-        setTasks(tasks.filter((task) => !task.completed));
-    }
+    const deleteTask = (index) => {
+        setShowConfirmModal(false);
+        setTasks(tasks.filter((_, i) => i !== index));
+    };
 
-    function applyFilter(filterValue) {
-        setFilter(filterValue);
-    }
+    const editTask = (index, newText) => {
+        setTasks(
+            tasks.map((task, i) =>
+                i === index ? {...task, text: newText} : task
+            )
+        );
+    };
 
-    function searchTasks(searchText) {
-        setSearch(searchText);
-    }
+    const changeTaskPriority = (index, newPriority) => {
+        setTasks(
+            tasks.map((task, i) =>
+                i === index ? {...task, priority: newPriority} : task
+            )
+        );
+    };
 
-    function saveTasksToLocalStorage() {
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-    }
+    const handleFilterChange = (e) => {
+        setFilter(e.target.value);
+    };
 
-    function loadTasksFromLocalStorage() {
-        const tasksJSON = localStorage.getItem('tasks');
-
-        if (tasksJSON) {
-            const loadedTasks = JSON.parse(tasksJSON);
-            setTasks(loadedTasks);
-        }
-    }
-
-    function handleTaskDragStart(event, taskId) {
-        setDraggedTask(taskId);
-    }
-
-    function handleTaskDragOver(event) {
-        event.preventDefault();
-    }
-
-    function handleTaskDragEnter(event) {
-        // Implement this function
-    }
-
-    function handleTaskDragLeave(event) {
-        // Implement this function
-    }
-
-    function handleTaskDrop(event, targetTaskId) {
-        // Implement this function
-    }
+    const handleSearchChange = (e) => {
+        setSearchText(e.target.value);
+    };
 
     const filteredTasks = tasks.filter((task) => {
-        switch (filter) {
-            case 'completed':
-                return task.completed;
-            case 'incomplete':
-                return !task.completed;
-            default:
-                return true;
+        if (filter === 'all') {
+            return true;
+        } else if (filter === 'completed') {
+            return task.completed;
+        } else if (filter === 'incomplete') {
+            return !task.completed;
         }
+        return false;
     });
 
     const searchedTasks = filteredTasks.filter((task) =>
-        task.text.toLowerCase().includes(search.toLowerCase())
+        task.text.toLowerCase().includes(searchText.toLowerCase())
     );
+
+
     return (
         <div className="App">
             <header>
-                <h1>To-Do list</h1>
+                <h1>Task Manager</h1>
             </header>
-            <SearchInput searchTasks={searchTasks} />
-            <TaskControls
-                addTask={addTask}
-                clearTasks={clearTasks}
-                applyFilter={applyFilter}
-            />
-            <DraggedTaskContext.Provider value={{ draggedTask, setDraggedTask }}>
+            <section className="add-task">
+                <input
+                    type="text"
+                    placeholder="New task..."
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    onKeyDown={(e) => e.key === 'Enter' && addTask()}
+                />
+                <button onClick={addTask}>Add</button>
+            </section>
+            <section className="filters">
+                <input
+                    type="text"
+                    placeholder="Search tasks..."
+                    value={searchText}
+                    onChange={handleSearchChange}
+                />
+                <select value={filter} onChange={handleFilterChange}>
+                    <option value="all">All</option>
+                    <option value="completed">Completed</option>
+                    <option value="incomplete">Incomplete</option>
+                </select>
+            </section>
+            <section className="list-container">
                 <TaskList
                     tasks={searchedTasks}
                     toggleTaskCompletion={toggleTaskCompletion}
-                    deleteTask={deleteTask}
+                    openConfirmModal={(index) => {
+                        setTaskIndexToDelete(index);
+                        setShowConfirmModal(true);
+                    }}
                     editTask={editTask}
-                    handleTaskDragStart={handleTaskDragStart}
-                    handleTaskDragOver={handleTaskDragOver}
-                    handleTaskDragEnter={handleTaskDragEnter}
-                    handleTaskDragLeave={handleTaskDragLeave}
-                    handleTaskDrop={handleTaskDrop}
+                    changeTaskPriority={changeTaskPriority}
                 />
-            </DraggedTaskContext.Provider>
+            </section>
+            {showConfirmModal && (
+                <ConfirmModal
+                    onClose={() => setShowConfirmModal(false)}
+                    onConfirm={() => deleteTask(taskIndexToDelete)}
+                />
+            )}
         </div>
     );
 }
 
+const TaskList = ({
+                      tasks,
+                      toggleTaskCompletion,
+                      openConfirmModal,
+                      editTask,
+                      changeTaskPriority,
+                  }) => {
+    return (
+        <ul>
+            {tasks.map((task, index) => (
+                <TaskItem
+                    key={index}
+                    task={task}
+                    index={index}
+                    toggleTaskCompletion={toggleTaskCompletion}
+                    openConfirmModal={openConfirmModal}
+                    editTask={editTask}
+                    changeTaskPriority={changeTaskPriority}
+                />
+            ))}
+        </ul>
+    );
+};
+
+const TaskItem = ({
+                      task,
+                      index,
+                      toggleTaskCompletion,
+                      openConfirmModal,
+                      editTask,
+                      changeTaskPriority,
+                  }) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedText, setEditedText] = useState(task.text);
+
+    const handleEditBlur = () => {
+        setIsEditing(false);
+        editTask(index, editedText);
+    };
+
+    return (
+        <li
+            className={`task-item ${task.completed ? 'completed' : ''} ${task.priority}-priority`}
+            onClick={() => !isEditing && toggleTaskCompletion(index)}
+        >
+            {isEditing ? (
+                <input
+                    type="text"
+                    value={editedText}
+                    onChange={(e) => setEditedText(e.target.value)}
+                    onBlur={handleEditBlur}
+                    autoFocus
+                />
+            ) : (
+                <span>{task.text}</span>
+            )}
+            <button
+                onClick={(e) => {
+                    e.stopPropagation();
+                    openConfirmModal(index);
+                }}
+            >
+                Delete
+            </button>
+            <button onClick={() => setIsEditing(!isEditing)}>Edit</button>
+            <select
+                value={task.priority}
+                onChange={(e) => changeTaskPriority(index, e.target.value)}
+            >
+                <option value="low">Low</option>
+                <option value="medium">Medium</option>
+                <option value="high">High</option>
+            </select>
+        </li>
+    );
+};
+
+const ConfirmModal = ({onClose, onConfirm}) => {
+    return (
+        <div className="confirm-modal">
+            <div className="modal-content">
+                <h3>Are you sure?</h3>
+                <div className="modal-buttons">
+                    <button onClick={onClose}>Cancel</button>
+                    <button onClick={onConfirm}>Confirm</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
 export default App;
+
